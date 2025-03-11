6eefb17bc9dd6fed6d6f299242b1b142502976ec
A <A@DESKTOP-06FGJVS>
2025-03-11 14:19:17 +0600

    Add project files.

---
+++ ProductCRUD/Models/Products.cs
@@ -0,0 +1,10 @@
+﻿namespace ProductCRUD.Models
+{
+    public class Products
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
+}
